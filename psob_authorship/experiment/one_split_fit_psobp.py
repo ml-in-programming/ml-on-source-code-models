8d58bca826adfb9f2460cf9d0d8cf67635c0251b
RamSaw <mikhail@pravilov.com>
2019-04-22 19:08:42 +0300

    Conducted some experiments. Looks like it is needed to write our own PSO.

--- psob_authorship/experiment/one_split_fit_psobp.py
+++ psob_authorship/experiment/one_split_fit_psobp.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 from sklearn import preprocessing
 from sklearn.model_selection import StratifiedKFold
-from torch import nn
+from torch import optim, nn
 
 from psob_authorship.experiment.utils import make_experiment_reproducible
 from psob_authorship.features.PsobDataset import PsobDataset
@@ -17,22 +17,28 @@ from psob_authorship.pso.PSO import PSO
 
 CONFIG = {
     'experiment_name': os.path.basename(__file__).split('.')[0],
-    'experiment_notes': "c1, c2 -> 1.49, max velocity range",
+    'experiment_notes': "change: without PSO",
     'number_of_authors': 40,
     'labels_features_common_name': "../calculated_features/extracted_for_each_file",
     'metrics': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],  # 9 is macro
+    'epochs': 5000,
+    'batch_size': 32,
     'early_stopping_rounds': 700,
+    'lr': 0.02,
     'n_splits': 10,
     'random_state': 4562,
+    'scoring': "accuracy",
     'criterion': nn.CrossEntropyLoss,
+    'optimizer': optim.SGD,
+    'momentum': 0.9,
+    'shuffle': True,
     'pso_options': {'c1': 1.49, 'c2': 1.49, 'w': 0.4},
     'pso_velocity_range': (0, 1),
     'n_particles': 100,
     'pso_iters': 400,
-    'optimizer': PSO,
-    'shuffle': True
+    'pso_optimizer': PSO
 }
-CONFIG['cv'] = StratifiedKFold(n_splits=CONFIG['n_splits'], random_state=CONFIG['random_state'], shuffle=True)
+CONFIG['cv'] = StratifiedKFold(n_splits=CONFIG['n_splits'], shuffle=True, random_state=CONFIG['random_state'])
 INPUT_FEATURES = torch.load(CONFIG['labels_features_common_name'] + "_features.tr").numpy()
 INPUT_LABELS = torch.load(CONFIG['labels_features_common_name'] + "_labels.tr").numpy()
 make_experiment_reproducible(CONFIG['random_state'])
@@ -51,35 +57,84 @@ def fit_model(file_to_print):
     train_index, test_index = next(CONFIG['cv'].split(INPUT_FEATURES, INPUT_LABELS))
     model = Model(len(CONFIG['metrics']))
     criterion = CONFIG['criterion']()
-    optimizer = CONFIG['optimizer'](model, criterion, CONFIG['pso_options'], CONFIG['n_particles'])
-
+    optimizer = CONFIG['optimizer'](model.parameters(), lr=CONFIG['lr'], momentum=CONFIG['momentum'])
     train_features, train_labels = INPUT_FEATURES[train_index], INPUT_LABELS[train_index]
     test_features, test_labels = INPUT_FEATURES[test_index], INPUT_LABELS[test_index]
     scaler = preprocessing.StandardScaler().fit(train_features)
     train_features = scaler.transform(train_features)
     test_features = scaler.transform(test_features)
 
+    """
     train_features = torch.from_numpy(train_features)
     train_labels = torch.from_numpy(train_labels)
     test_features = torch.from_numpy(test_features)
     test_labels = torch.from_numpy(test_labels)
     pso_bounds = (np.full((model.dimensions,), CONFIG['pso_velocity_range'][0]),
                   np.full((model.dimensions,), CONFIG['pso_velocity_range'][1]))
-    loss, _ = optimizer.optimize(train_features, train_labels, CONFIG['pso_iters'], pso_bounds)
+    pso_optimizer = CONFIG['pso_optimizer'](model, criterion, CONFIG['pso_options'], CONFIG['n_particles'])
+    loss, _ = pso_optimizer.optimize(train_features, train_labels, CONFIG['pso_iters'], pso_bounds)
     print_info("Loss after PSO optimizing = " + str(loss))
+    """
 
+    trainloader = torch.utils.data.DataLoader(
+        PsobDataset(train_features, train_labels, CONFIG['metrics']),
+        batch_size=CONFIG['batch_size'], shuffle=CONFIG['shuffle'], num_workers=2
+    )
+    testloader = torch.utils.data.DataLoader(
+        PsobDataset(test_features, test_labels, CONFIG['metrics']),
+        batch_size=CONFIG['batch_size'], shuffle=CONFIG['shuffle'], num_workers=2
+    )
     best_accuracy = -1.0
+    current_duration = 0
+    for epoch in range(CONFIG['epochs']):
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in testloader:
+                features, labels = data
+                outputs = model(features)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        accuracy = correct / total
+        if best_accuracy >= accuracy:
+            current_duration += 1
+        else:
+            current_duration = 0
+        best_accuracy = max(best_accuracy, accuracy)
+        if current_duration > CONFIG['early_stopping_rounds']:
+            print_info("On epoch " + str(epoch) + " training was early stopped")
+            break
+        if epoch % 10 == 0:
+            logger.info("CHECKPOINT EACH 10th EPOCH" + str(epoch) + ": " + str(accuracy))
+        if epoch % 100 == 0:
+            print_info("CHECKPOINT EACH 100th EPOCH " + str(epoch) + ": current accuracy " + str(accuracy) + " , best "
+                       + str(best_accuracy))
+        logger.info(str(epoch) + ": " + str(accuracy))
+
+    logger.info('Finished Training')
+
     correct = 0
     total = 0
     labels_dist = torch.zeros(CONFIG['number_of_authors'])
     labels_correct = torch.zeros(CONFIG['number_of_authors'])
-    outputs = model(test_features)
-    _, predicted = torch.max(outputs.data, 1)
-    total += test_labels.size(0)
-    correct += (predicted == test_labels).sum().item()
-    for i, label in enumerate(test_labels):
-        labels_dist[label] += 1
-        labels_correct[label] += predicted[i] == test_labels[i]
+    with torch.no_grad():
+        for data in testloader:
+            features, labels = data
+            outputs = model(features)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            for i, label in enumerate(labels):
+                labels_dist[label] += 1
+                labels_correct[label] += predicted[i] == labels[i]
     print_info('Best accuracy: ' + str(max(best_accuracy, correct / total)))
     print_info('Final accuracy of the network: %d / %d = %d %%' % (correct, total, 100 * correct / total))
     print_info("Correct labels / labels for each author:\n" + str(torch.stack((labels_correct, labels_dist), dim=1)))
---
+++ psob_authorship/pso/DecreasingWeightPsoOptimizer.py
@@ -0,0 +1,3 @@
+class DecreasingWeightPsoOptimizer:
+    def __init__(self, ) -> None:
+        super().__init__()
